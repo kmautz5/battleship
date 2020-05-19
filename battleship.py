a44b7eed807f72e16f4295133d55563bce0e059e
Kelly Ogawa <kmautz5@gmail.com>
2020-05-19 10:16:15 -0400

    First commit

---
+++ battleship.py
@@ -0,0 +1,54 @@
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+#print ship_row
+#print ship_col
+
+for turn in range(4):
+  
+  print "Turn", turn + 1
+  raw_row = raw_input("Guess Row: ")
+  raw_col = raw_input("Guess Column: ")
+  if raw_row.isdigit() and raw_col.isdigit():
+    guess_row = int(raw_row)
+    guess_col = int(raw_col)
+  
+    if guess_row == ship_row and guess_col == \
+      ship_col:
+      print "Congratulations! You sank my battleship!"
+      break  
+    if turn == 3:
+      print "Game Over"
+    else:
+      if guess_row not in range(5) or \
+        guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+      elif board[guess_row][guess_col] == "X":
+        print( "You guessed that one already." )
+      else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+    
+  else:
+     print "Invalid input! Terminating!"
+     break
+
+print_board(board)
