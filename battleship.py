e04333af5dc227ca0b76aac94494cac5f06d72e4
Kelly Ogawa <kmautz5@gmail.com>
2020-05-19 10:26:30 -0400

    Second working version. Added rematch functionality and increased board size.

--- battleship.py
+++ battleship.py
@@ -2,14 +2,14 @@ from random import randint
 
 board = []
 
-for x in range(0, 5):
-  board.append(["O"] * 5)
+for x in range(0, 10):
+  board.append(["O"] * 10)
 
 def print_board(board):
   for row in board:
     print " ".join(row)
 
-print_board(board)
+#print_board(board)
 
 def random_row(board):
   return randint(0, len(board) - 1)
@@ -17,38 +17,54 @@ def random_row(board):
 def random_col(board):
   return randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
-#print ship_row
-#print ship_col
 
-for turn in range(4):
+game_is_finished = False
+turn = 0
+
+while game_is_finished is False:
   
+  print_board(board)
   print "Turn", turn + 1
+  
+  if turn == 0:
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    
   raw_row = raw_input("Guess Row: ")
-  raw_col = raw_input("Guess Column: ")
+  raw_col = raw_input("Guess Column: ") #check if raw input is a digit, also catches empty string
   if raw_row.isdigit() and raw_col.isdigit():
     guess_row = int(raw_row)
     guess_col = int(raw_col)
   
     if guess_row == ship_row and guess_col == \
-      ship_col:
+        ship_col:
       print "Congratulations! You sank my battleship!"
-      break  
-    if turn == 3:
-      print "Game Over"
+      turn = 3 #this triggers rematch
     else:
-      if guess_row not in range(5) or \
-        guess_col not in range(5):
-        print "Oops, that's not even in the ocean."
+      if guess_row not in range(10) or \
+         guess_col not in range(10):
+         print "Oops, that's not even in the ocean."
+         turn += 1
       elif board[guess_row][guess_col] == "X":
-        print( "You guessed that one already." )
+         print( "You guessed that one already." )
+         turn += 1
       else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
-    
+         print "You missed my battleship!"
+         board[guess_row][guess_col] = "X"
+         turn += 1
+    #this is where invalid raw input terminates program
   else:
      print "Invalid input! Terminating!"
-     break
+     game_is_finished = True #exits the loop
+###
+  if turn == 3:
+    print "Game Over"
+    rematch = raw_input("Do you want a rematch? ")
+    if rematch == "Y" or rematch =="y" or \
+        rematch == "yes":
+      turn = 0 #if user wants a rematch, set turn back to 0
+    else:
+      print "I guess not.."
+      game_is_finished = True
 
-print_board(board)
+#after the loop
