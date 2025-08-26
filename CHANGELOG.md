98a51de802c57282ae84590c00ef8a4bc73f69ea
kmautz5 <kmautz5@gmail.com>
2025-08-26 15:03:58 -0400

    new working version of battleship; added icon to repo;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # Version History
 
-0.0.1 edits to fix type error
+0.0.1 paradigm shift and complete rework: edits to fix type error, changed place ship function to work two-dimensionally,
 0.0.0 initial release 2022
--- battleship.py
+++ battleship.py
@@ -1,114 +1,112 @@
-#from random import randint, randrange   imports only these two functions...
-from random import * #import all from random
+from random import *
 
+# board setup
 board = []
-
-for x in range(0, 10):
+for x in range(10):
   board.append(["O"] * 10)
-
-
 
 def print_board(board):
   for row in board:
     print(" ".join(row))
 
-#print_board(board)
+def random_coords(board):
+    return randint(0, len(board) - 1), randint(0, len(board[0]) - 1)
 
-def random_row(board):
-  return randint(0, len(board) - 1) #prevents it from choosing a value beyond the board
+def place_ship(board, shipSize):
+  # place a ship of a given shipSize on the board
+  while True:
+    # choose a random orientation to place a ship
+    orientation = choice(['horizontal', 'vertical'])
+    if orientation == 'horizontal':
+      ship_row = randint(0, len(board) - 1)
+      ship_col = randint(0, len(board[0]) - shipSize)
+      coords = [(ship_row, ship_col + i) for i in range(shipSize)]
+    else: # vertical
+      ship_row = randint(0, len(board) - shipSize)
+      ship_col = randint(0, len(board[0]) - 1)
+      coords = [(ship_row + i, ship_col) for i in range(shipSize)]
 
-def random_col(board):
-  return randint(0, len(board[0]) - 1)
+    # check for conflicts with other ships, if so restart at while true
+    conflict = False
+    for r, c in coords:
+      if board[r][c] == "S":
+        conflict = True
+        break
+    if not conflict:
+      for r, c in coords:
+        board[r][c] = "S" # S marks where the ship is
+      print(coords)
+      return coords
 
-
-game_is_finished = False
-turn = 0
-ship_sank = 0
-
-while game_is_finished is False:
+def main_game():
+  global board
+  board = [["O"] * 10 for _ in range(10)]
   
-  print_board(board)
-  print("Turn", turn + 1)
+  # place ships (1x1 and 2x2)
+  ship1_coords = place_ship(board, 1)
+  ship2_coords = place_ship(board, 2)
   
-  if turn == 5 or ship_sank == 2: #if player loses or wins, exit the while loop
-    game_is_finished = True
+  # Hide the ships for the player
+  for r in range(10):
+    for c in range(10):
+      if board[r][c] == "S":
+        board[r][c] = "O"
 
-  if turn == 0:
-    #generate 1x1 ship at random location on the board
-    ship1_row = random_row(board)
-    ship1_col = random_col(board)
-      
-    #generate 2x2 ship at random location on the board
-    #right now this may randomely choose the same coordinates..
-    ship2_row_firstpoint = random_row(board)
-    ship2_col_firstpoint = random_col(board)
-    #firstpoint = (ship2_col_firstpoint,ship2_row_firstpoint)
-
-    #not getting second point off the board!
-    if ship2_row_firstpoint == 0: #[?,0]
-      ship2_row_secondpoint = randrange(0,1)
-      if ship2_col_firstpoint == 0 and ship2_row_secondpoint == 0: #[0,0] [?,0]
-        ship2_col_secondpoint = 1 #[0,0] [1,0]
-      elif ship2_col_firstpoint == 0 and ship2_row_secondpoint == 1: #[0,0] [?,1]
-        ship2_col_secondpoint = 0 #[0,0] [0,1]
-    #not getting second point off the board!
-    elif ship2_row_firstpoint == 9: #[?,9]
-      ship2_row_secondpoint = randrange(8,9)
-      if ship2_col_firstpoint == 9 and ship2_row_secondpoint == 9: #[9,9] [?,9]
-        ship2_col_secondpoint = 8 #[9,9] [8,9]
-      elif ship2_col_firstpoint == 9 and ship2_row_secondpoint == 8: #[9,9] [?,8]
-        ship2_col_secondpoint = 9 #[9,9] [9,8]
-    else:
-      ship2_row_secondpoint = randrange(ship2_row_firstpoint-1,ship2_row_firstpoint+1) #get the nearest neighboring point
-      if ship2_row_secondpoint == ship2_row_firstpoint:
-        ship2_col_secondpoint = choice(ship2_row_firstpoint-1,ship2_row_firstpoint+1) #ship2 is horizontal
-      else:
-        ship2_col_secondpoint = ship2_col_firstpoint #ship2 is vertical
-
-  raw_row = input("Guess Row: ")
-  raw_col = input("Guess Column: ") #check if raw input is a digit, also catches empty string
-  if raw_row.isdigit() and raw_col.isdigit():
-    guess_row = int(raw_row)
-    guess_col = int(raw_col)
+  ships_remaining = [ship1_coords, ship2_coords]
+  turns_left = 5
   
+  while turns_left > 0 and len(ships_remaining) > 0:
+    print_board(board)
+    print("Turns left:", turns_left)
     
-    if guess_row not in list(range(10)) or guess_col not in list(range(10)):
-      print("Oops, that's not even in the ocean.") 
-      turn += 1
-
-    if guess_row == ship1_row and guess_col == ship1_col:
-      print("You sank my battleship!")
-      turn += 1
-      ship_sank += 1
-
-    if guess_row == ship2_row_firstpoint and guess_col == ship2_col_firstpoint:
-      print("I've been hit!")
-      turn += 1
-      
-      
+    try:
+      guess_row = int(input("Guess Row: "))
+      guess_col = int(input("Guess Column: "))
+    except ValueError:
+      print("Invalid input! Please enter a number.")
+      continue
+        
+    if not (0 <= guess_row < 10 and 0 <= guess_col < 10):
+      print("Oops, that's not even in the ocean.")
+      turns_left -= 1
+      continue
+        
     if board[guess_row][guess_col] == "X":
-       print( "You guessed that one already." )
-       turn += 1
-    else:
-       print("You missed my battleship!")
-       board[guess_row][guess_col] = "X"
-       turn += 1
-    
-  else:
-     print("Invalid input! Terminating!")
-     game_is_finished = True #exits the loop
+      print("You guessed that one already.")
+      turns_left -= 1
+      continue
 
-#end of game prompts
-  if ship_sank == 2:
+    is_hit = False
+    ship_sank = -1
+    for i, ship in enumerate(ships_remaining):
+      if (guess_row, guess_col) in ship:
+        ship.remove((guess_row, guess_col))
+        if not ship:
+          print("You sank a battleship!")
+          ship_sank = i
+          board[guess_row][guess_col] = "X"
+          is_hit = True
+        else:
+          print("I've been hit!")
+          board[guess_row][guess_col] = "H"
+          is_hit = True
+        break
+    
+    if is_hit and ship_sank != -1:
+      ships_remaining.pop(ship_sank)
+    elif not is_hit:
+      print("You missed my battleship!")
+      board[guess_row][guess_col] = "X"
+
+    turns_left -= 1
+
+  print_board(board)
+  if not ships_remaining:
     print("Congratulations! You won!")
   else:
     print("Game Over")
-    rematch = input("Do you want a rematch? ")
-    if rematch == "Y" or rematch =="y" or rematch == "yes":
-      print("Regenerating board!")
-      turn = 0 #if user wants a rematch, set turn back to 0
-    else:
-      print("I guess not..")
-      game_is_finished = True
 
-#after the loop
+rematch = "yes"
+while rematch.lower() in ["y", "yes"]:
+  main_game()
+  rematch = input("Do you want a rematch?")
